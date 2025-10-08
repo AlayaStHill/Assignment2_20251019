fc987cfe74dabd4a7c1273196de396b6bcc09456
AlayaStHill <alaya.st.hill@gmail.com>
2025-10-08 14:49:28 +0200

    feat: add SetProduct to ProductEditViewModel

--- Presentation/ViewModels/ProductEditViewModel.cs
+++ Presentation/ViewModels/ProductEditViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Domain.Entities;
 
 namespace Presentation.ViewModels;
 
 public class ProductEditViewModel : ObservableObject
 {
+    private void SetProduct(Product product)
+    {
+
+    }
 }
 //  visa Id som inte kan editeras i edit-view
