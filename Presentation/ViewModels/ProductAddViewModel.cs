a00a0d9953beab553cc159279a117080e54bafc0
AlayaStHill <alaya.st.hill@gmail.com>
2025-10-09 09:12:35 +0200

    feat: ProductAddViewModel setup complete

--- Presentation/ViewModels/ProductAddViewModel.cs
+++ Presentation/ViewModels/ProductAddViewModel.cs
@@ -1,7 +1,40 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using ApplicationLayer.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Domain.Entities;
+using Presentation.Interfaces;
 
 namespace Presentation.ViewModels;
 
-public class ProductAddViewModel : ObservableObject
+public partial class ProductAddViewModel : ObservableObject
 {
+    private readonly IViewNavigationService _viewNavigationService;
+    private readonly IProductService _productService;
+
+    public ProductAddViewModel(IViewNavigationService viewNavigationService, IProductService productService)
+    {
+        _viewNavigationService = viewNavigationService;
+        _productService = productService;
+    }
+
+    [ObservableProperty]
+    private Product _productData = new();
+
+    [ObservableProperty]
+    private string _title = "Ny Produkt";
+
+    [ObservableProperty]
+    private string _statusMessage;
+
+    [ObservableProperty]
+    private string _statusColor;
+
+    private void Save()
+    {
+
+    }
+
+    private void Cancel()
+    {
+
+    }
 }
